e97f18f92e7d034221dead23413bf1d098cbf118
Le Thanh Cong (PM) <conglt@nms.vn>
2021-10-28 17:07:35 +0700

    oke

--- src/main/java/com/shoppingbackend/exceptions/AccountLockedException.java
+++ src/main/java/com/shoppingbackend/exceptions/AccountLockedException.java
@@ -2,7 +2,7 @@ package com.shoppingbackend.exceptions;
 
 public class AccountLockedException extends Exception {
     public AccountLockedException() {
-        super("Tài khoản của bạn đã bị khóa, vui lòng liên hệ Admin để mở: 0342910909");
+        super("Tài khoản của bạn đã bị khóa !");
     }
 
     public AccountLockedException(String message) {
